a5997f496f958045d45c5ad9dd46c8613891dbcb
camilaherin125 <camilitahering@gmail.com>
2026-05-25 09:02:32 -0400

    Agregando CORS

--- src/middlewares/error.middleware.js
+++ src/middlewares/error.middleware.js
@@ -1,11 +1,15 @@
+// Middleware para manejar errores del servidor
 const manejarErrores = (err, req, res, next) => {
 
+    // Muestra el error en la consola
     console.error(err.stack);
 
+    // Devuelve respuesta con estado 500
     res.status(500).json({
         mensaje: 'Error interno del servidor'
     });
 
 };
 
+// Exporta el middleware para usarlo en otras partes
 module.exports = manejarErrores;
--- src/servidor.js
+++ src/servidor.js
@@ -1,23 +1,41 @@
+// Carga las variables de entorno desde el archivo .env
 require('dotenv').config();
 
+// Importa el framework Express
 const express = require('express');
+const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('../swagger');
 
+// Importa las rutas de materias
 const materiasRutas = require('./rutas/materias.rutas');
 
+// Importa el middleware para manejar errores
 const manejarErrores = require('./middlewares/error.middleware');
 
+// Crea la aplicación de Express
 const app = express();
+app.use(cors());
 
+// Permite que el servidor entienda datos en formato JSON
 app.use(express.json());
 
+// Define la ruta principal para acceder a las materias
+// Todas las rutas de materias comenzarán con /api/materias
 app.use('/api/materias', materiasRutas);
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
+// Usa el middleware de manejo de errores
 app.use(manejarErrores);
 
+// Define el puerto donde correrá el servidor
+// Usa el puerto del archivo .env o el 3000 por defecto
 const PORT = process.env.PORT || 3000;
 
+// Inicia el servidor
 app.listen(PORT, () => {
 
+    // Muestra un mensaje en consola cuando el servidor funciona correctamente
     console.log(`Servidor funcionando en puerto ${PORT}`);
 
 });
